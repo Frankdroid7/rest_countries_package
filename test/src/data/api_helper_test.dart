3ccfaeacbd94820b532c98ae6fe4970b9ffad149
motola <olutolamoses@gmail.com>
2025-08-16 19:10:00 +0100

    test: add translation endpoint test

--- test/src/data/api_helper_test.dart
+++ test/src/data/api_helper_test.dart
@@ -171,6 +171,21 @@ void main() {
         expect(result, isA<List<Map<String, dynamic>>>());
       },
     );
+
+    test(
+      'GIVEN translation endpoint, WHEN callAPI is called with valid translation, THEN return a List<Map<String, dynamic>>',
+      () async {
+        final String url = 'https://restcountries.com/v3.1/translation/france';
+        when(() => mockApiHelper.callAPI(apiUrl: url)).thenAnswer((_) =>
+            Future<List<Map<String, dynamic>>>.value(
+                List<Map<String, dynamic>>.from(jsonDecode(mockData))));
+
+        List<Map<String, dynamic>> result =
+            await mockApiHelper.callAPI(apiUrl: url);
+
+        expect(result, isA<List<Map<String, dynamic>>>());
+      },
+    );
   });
 }
 
