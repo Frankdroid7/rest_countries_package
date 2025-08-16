4cd4895f1c0c3edd9671a98cbf1b54a8015784da
motola <olutolamoses@gmail.com>
2025-08-16 11:45:00 +0100

    test: add currency endpoint test

--- test/src/data/api_helper_test.dart
+++ test/src/data/api_helper_test.dart
@@ -95,6 +95,21 @@ void main() {
         expect(result, isA<List<Map<String, dynamic>>>());
       },
     );
+
+    test(
+      'GIVEN currency endpoint, WHEN callAPI is called with valid currency, THEN return a List<Map<String, dynamic>>',
+      () async {
+        final String url = 'https://restcountries.com/v3.1/currency/usd';
+        when(() => mockApiHelper.callAPI(apiUrl: url)).thenAnswer((_) =>
+            Future<List<Map<String, dynamic>>>.value(
+                List<Map<String, dynamic>>.from(jsonDecode(mockData))));
+
+        List<Map<String, dynamic>> result =
+            await mockApiHelper.callAPI(apiUrl: url);
+
+        expect(result, isA<List<Map<String, dynamic>>>());
+      },
+    );
   });
 }
 
